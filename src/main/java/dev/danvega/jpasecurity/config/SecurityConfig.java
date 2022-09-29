1cafcc60c7f866a17a4c117654a2ad9c2397c652
Dan Vega <danvega@gmail.com>
2022-09-29 11:10:17 -0400

    updating the password encoder to use BCrypt

--- src/main/java/dev/danvega/jpasecurity/config/SecurityConfig.java
+++ src/main/java/dev/danvega/jpasecurity/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
